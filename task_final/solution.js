65e67e63201930939c66a39131a5bc25ecc38c24
Regina Shaekhova <incubi116@gmail.com>
2022-01-10 22:57:51 +0300

    final task done?

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,9 +39,8 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        let itemOfGoods = {title: title, count: count};
-      
-        data.goods.push(goods[i].itemOfGoods);
+        
+        data.goods[i] = {title: goods[i].title, count: goods[i].count}     
     }
     
     data.client = name + " " + phone;
